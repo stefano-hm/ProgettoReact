b9468631802f4180244c3a6d3eb7a18a4b426159
Your Name <you@example.com>
2025-04-16 15:17:37 +0200

    aggiunta logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
 import '../styles/Header.css'
+import '../styles/Logo.css'
 
 
 export default function Header() {
     return (
       <nav className="nav">
-        <div className="nav-logo">🧘 Medita</div>
+        <div className="nav-logo">
+          <img src="/images/logo.PNG" alt="Logo Medita" className="nav-logo-img" />
+        </div>
         <div className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/guide">Guide</Link>
